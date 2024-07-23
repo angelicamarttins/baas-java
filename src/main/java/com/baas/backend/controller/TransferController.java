bdc006991a97ddbb265ab82801f4adb2604eb137
Angélica Martins <marttinsangelica@gmail.com>
2024-07-22 21:05:24 -0300

    Refactor: Fix TransferRequestDto and add log in TranferController

--- src/main/java/com/baas/backend/controller/TransferController.java
+++ src/main/java/com/baas/backend/controller/TransferController.java
@@ -1,16 +1,27 @@
 package com.baas.backend.controller;
 
+import com.baas.backend.data.dto.TransferRequestDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/transfer")
+@Slf4j
 public class TransferController {
 
   @PostMapping
-  public ResponseEntity<String> transfer() {
+  public ResponseEntity<String> transfer(@RequestBody TransferRequestDto transferRequest) {
+    log.info(
+      "Starting transfer amount between accounts. ClientId: {}, SourceAccountId: {}, TargetAccountId: {}",
+      transferRequest.clientId(),
+      transferRequest.accounts().sourceAccountId(),
+      transferRequest.accounts().targetAccountId()
+    );
+
     return ResponseEntity.ok("Hello, world");
   }
 
--- src/main/java/com/baas/backend/model/Accounts.java
+++ src/main/java/com/baas/backend/model/Accounts.java
@@ -2,5 +2,5 @@ package com.baas.backend.model;
 
 import java.util.UUID;
 
-public record Accounts(UUID clientSourceId, UUID clientTargetId) {
+public record Accounts(UUID sourceAccountId, UUID targetAccountId) {
 }
